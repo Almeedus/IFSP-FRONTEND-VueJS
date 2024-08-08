a1f78d52674ffcfe0b20c0e203eccbce258365c1
Eduardo Almeida <almeedusa@gmail.com>
2024-08-08 18:21:17 -0300

    (UPDATING / CREATING FILES): Creating a ProductList component and using it on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div id="app">
-    
+    <h1>Carrinho de Compras </h1>
+    <ProductList @add-to-cart="addToCart" />
+    <Cart :cart="cart" @remove-from-cart="removeFromCart" />
   </div>
 </template>
 
 <script>
+import ProductList from './components/ProductList.vue';
 
+export default {
+  components: {
+    ProductList,
+  },
+  data() {
+    return {
+      cart: [],
+    };
+  },
+  methods: {
+    addToCart(product) {
+      const item = this.cart.find(item => item.id === product.id);
+      if (item) {
+        item.quantity++;
+      } else {
+        this.cart.push({ ...product, quantity: 1 });
+      }
+    },
+    removeFromCart(productId) {
+      this.cart = this.cart.filter(item => item.id !== productId);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
